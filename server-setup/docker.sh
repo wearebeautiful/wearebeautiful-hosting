04bd979697ce580c7263748bc977303a5cebcf85
Robert Kaye <rob@musicbrainz.org>
2020-03-07 00:40:26 +0100

    Add more docker stuff to unstupid docker

--- server-setup/docker.sh
+++ server-setup/docker.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Stop docker from changing iptables
+mkdir -p /etc/docker/
+cat <<EOF >daemon.json
+{
+    "dns": ["8.8.8.8", "8.8.4.4"],
+    "iptables": false,
+    "log-driver": "json-file",
+    "log-opts": {
+      "max-size": "10m",
+      "max-file": "3"
+    }
+}
+EOF
+sudo mv -f daemon.json /etc/docker/daemon.json
+
+apt-get install -y \
+     apt-transport-https \
+     ca-certificates \
+
 apt-get install -y \
     apt-transport-https \
     ca-certificates \
@@ -17,3 +36,31 @@ add-apt-repository \
 apt-get update
 
 apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# http://www.acervera.com/blog/2016/03/05/ufw_plus_docker
+if [ -e /etc/default/ufw ]; then
+	sudo sed -i 's/^DEFAULT_FORWARD_POLICY=.*$/DEFAULT_FORWARD_POLICY="ACCEPT"/' /etc/default/ufw
+	sudo ./docker_ufw_rules.sh
+	echo "Please reboot, ufw rules were modified"
+else
+	echo "Now exit and relog as $USER"
+fi
+
+if ! grep -q '^# Rules needed by docker' /etc/ufw/before.rules; then
+cat <<EOF > /tmp/before.rules
+# Rules needed by docker (do not modify this line)
+# See https://svenv.nl/unixandlinux/dockerufw
+*nat
+:POSTROUTING ACCEPT [0:0]
+-A POSTROUTING ! -o docker0 -s 172.17.0.0/16 -j MASQUERADE
+COMMIT
+EOF
+cat /etc/ufw/before.rules >> /tmp/before.rules
+
+cp /etc/ufw/before.rules /etc/ufw/before.rules.bak
+cat /tmp/before.rules > /etc/ufw/before.rules
+rm -f /tmp/before.rules
+diff -u /etc/ufw/before.rules.bak /etc/ufw/before.rules
+fi
+ufw allow in on docker0 from 172.17.0.0/16 to any
+ufw allow out on docker0 from any to 172.17.0.0/16
