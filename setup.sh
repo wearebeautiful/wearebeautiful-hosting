d4a0ff7b03e6ad6eabb23784b77f939b2cf39ec7
Robert Kaye <rob@musicbrainz.org>
2020-03-06 21:31:36 +0100

    Add a wab user for the website

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ echo "Settting up $HOSTNAME..."
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y build-essential git fail2ban ufw vim
+apt-get install -y build-essential git fail2ban ufw vim python3-dev python3-pip
 
 ./firewall.sh
 ./hostname.sh $HOSTNAME
@@ -25,6 +25,10 @@ apt-get install -y build-essential git fail2ban ufw vim
 adduser --disabled-password --gecos "Robert Kaye" robert
 adduser robert sudo
 adduser robert docker
+
+adduser --disabled-password --gecos "WAB website" wab
+adduser wab sudo
+adduser wab docker
 
 echo "%sudo   ALL=(ALL:ALL) NOPASSWD:ALL"
 read -p "Now set up sudo and make sure to fix the %sudo line to look like the one above"
