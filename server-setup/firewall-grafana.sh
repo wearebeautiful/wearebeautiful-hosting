3df9dba7803ca465323ba9d6a18ce7e957f93866
Robert Kaye <rob@musicbrainz.org>
2020-08-31 15:05:07 +0200

    Use the docker-ufw script since the other method doesn't work.

--- server-setup/firewall-grafana.sh
+++ server-setup/firewall-grafana.sh
@@ -3,7 +3,7 @@
 # only log ufw stuff to one file (/var/log/ufw.log by default)
 sed -i 's/^#& stop/\& stop/' /etc/rsyslog.d/20-ufw.conf && systemctl restart rsyslog
 
-ufw allow ssh
+ufw route allow proto tcp from any to any port 443
 ufw allow from 135.181.45.162 to any port 8086
 ufw allow from 95.217.160.238 to any port 8086
 
--- server-setup/firewall.sh
+++ server-setup/firewall.sh
@@ -3,6 +3,10 @@
 # only log ufw stuff to one file (/var/log/ufw.log by default)
 sed -i 's/^#& stop/\& stop/' /etc/rsyslog.d/20-ufw.conf && systemctl restart rsyslog
 
+wget -O /usr/local/bin/ufw-docker https://github.com/chaifeng/ufw-docker/raw/master/ufw-docker
+chmod +x /usr/local/bin/ufw-docker
+ufw-docker install
+
 ufw route allow proto tcp from any to any port 22
 
 ufw enable
